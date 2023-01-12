0ec5ec8b8f0316107059ce093ed0545e5b52f9a3
Gabriel Henrique <gabrielhenri.sg@gmail.com>
2023-01-12 00:07:57 -0300

    if in column loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import plotly.express as px
 #import database
 table = pd.read_csv("telecom_users.csv")
 
+
 #-----------------------------------------------------------------------------------------------------------------------
 #solving table problems - wrong value recognition and empty values
 
@@ -12,7 +13,7 @@ table["TotalGasto"] = pd.to_numeric(table["TotalGasto"], errors="coerce")
 #eraise columns that all values are empty
 table = table.dropna(how="all",axis=1)
 #eraise lines that have at least one value empty
-table = table.dropna(how="any",axis=0)
+table = table.dropna()
 #remove useless data 
 table = table.drop("Unnamed: 0", axis=1)
 
@@ -27,6 +28,7 @@ print(table["Churn"].value_counts(normalize=True).map("{:.1%}".format))
 
 #create graph
 for column in table.columns:
-    graph = px.histogram(table, x=column, color="Churn", text_auto=True)
-    #show graph
-    graph.show()
+    if column != 'IDCliente':
+        graph = px.histogram(table, x=column, color="Churn", text_auto=True)
+        #show graph
+        graph.show()
